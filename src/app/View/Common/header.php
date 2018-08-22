9cc0f5586b5e6b0770db68dbcebf69c4a0656246
RonieGSS <ronievincent.gss@gmail.com>
2018-08-22 11:23:12 +0800

    Added assets and favicon

--- src/app/View/Common/header.php
+++ src/app/View/Common/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
 	<title>Final Examination</title>
+	<link type="text/css" rel="stylesheet" href="lib/Assets/bootstrap/css/bootstrap.min.css">
+	<link rel="icon" href="./favicon.ico"> 
 </head>
 <body>
