22b570e4452fcb754f673fd7f5020a521acb0fa8
RonieGSS <ronievincent.gss@gmail.com>
2018-08-23 08:54:49 +0800

    Added unnecessary codes

--- src/app/View/home.php
+++ src/app/View/home.php
@@ -7,7 +7,7 @@
 	<h2>Home</h2>
 	<p>This is the home view page located at app/View folder.</p>
 	<p>Your final examination starts... NOW!</p>
-	<p>Please remove unnecessary codes like this -> <?= $isDBConnected ?></p>
+	<p>Oh Wait! Please remove unnecessary codes like this -> <?= $isDBConnected ?></p>
 	<p>Good Luck!</p>
 </div>
 
