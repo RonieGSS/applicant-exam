c8d5650d07b877a870780b8885cc1150ff42a63e
RonieGSS <ronievincent.gss@gmail.com>
2018-08-23 08:53:58 +0800

    Added unnecessary codes

--- src/app/Model/UsersModel.php
+++ src/app/Model/UsersModel.php
@@ -5,4 +5,20 @@ namespace App\Model;
 class UsersModel extends TableModel
 {
 	// Start Coding!
+
+	/**
+	 * Contains database connection
+	 * @var dbConn PDO Object
+	 */
+	private $dbConn;
+
+	public function __construct()
+	{
+		$this->dbConn = $this->conn();
+	}
+
+	public function getConn()
+	{
+		return $this->dbConn;
+	}
 }
