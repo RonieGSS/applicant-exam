2b7da840d51cdf13ef18a3aa4cbe95fcb19236e5
RonieGSS <ronievincent.gss@gmail.com>
2018-08-31 13:13:56 +0800

    Added additional info

--- src/app/Config/layout.php
+++ src/app/Config/layout.php
@@ -5,12 +5,14 @@ use Lib\Controller\ViewController;
 $viewTemplateSetting = new ViewController();
 
 /**
+ * The current header can be found at app/View/Common/header.php
  * You can set customized header
  * (ex. $viewTemplateSetting->setHeader(myheader.php);)
  */
 $viewTemplateSetting->setHeader();
 
 /**
+ * The current footer can be found at app/View/Common/footer.php
  * You can set customized footer
  * (ex. $viewTemplateSetting->setFooter(myfooter.php);)
  */
